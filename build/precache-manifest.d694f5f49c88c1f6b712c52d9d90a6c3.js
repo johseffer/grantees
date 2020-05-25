4fd6f60ba1488e76bc0abbded6ace0843f4ecb9d
Johseffer <johseffer@gmail.com>
2020-05-25 17:45:18 -0300

    including expressjs and setting heroku deploy

---
+++ build/precache-manifest.d694f5f49c88c1f6b712c52d9d90a6c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3ecf0af92095868a2569105d1117c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b38df14485ce88a5d970",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "56e28299fc7fcd822e4c",
+    "url": "/static/js/2.6ad5ad6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6ad5ad6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b38df14485ce88a5d970",
+    "url": "/static/js/main.ced3ea26.chunk.js"
+  },
+  {
+    "revision": "8d57edb89ffbc9f9a16d",
+    "url": "/static/js/runtime-main.b8148af6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
