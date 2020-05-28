7b3e049de231ef33396cb947a7a32d8899008cba
Johseffer <johseffer@gmail.com>
2020-05-28 11:25:32 -0300

    editing nav component to use material ui tab component

--- src/components/nav/nav.component.js
+++ src/components/nav/nav.component.js
@@ -1,20 +1,46 @@
 import React from 'react'
 import { A } from 'hookrouter'
+import { AppBar, Tabs, Tab } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
 
 import transfeeraIcon from './images/transfeera-logo-verde.svg';
 
 import './nav.component.scss'
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: '#1FBFAE',
+        display: 'flex',
+        flexDirection: 'column',
+        paddingLeft: '25px'
+    },
+    indicator: {
+        backgroundColor: 'white',
+    }
+}));
+
 const Nav = () => {
+    const [value, setValue] = React.useState(0);
+    
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const classes = useStyles();
+    
     return (
         <>
             <div className="container">
                 <div className="header">
                     <img className="iconeTransfera" alt="" src={transfeeraIcon} />
                 </div>
-                <div className="nav">
-                    <A href="/">Seus Favorecidos</A>
-                </div> 
+                <AppBar className={classes.root} position="static">
+                    <Tabs classes={{ indicator: classes.indicator }} value={value} onChange={handleChange}>
+                        <Tab style={{ textTransform: 'none' }} label="Seus Favorecidos" href="/" component="a"  id="nav-tab-grantees" />
+                    </Tabs>
+                </AppBar>
             </div>
         </>
     )
