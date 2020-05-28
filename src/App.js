4d0ca1fd59d9e1449d71d65314f21908ac192a46
Johseffer <johseffer@gmail.com>
2020-05-28 16:22:53 -0300

    removing unnecessary display flex of app

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import routes from './routes'
 
 import Nav from './components/nav/nav.component'
 
-import styles from './App.scss'
+import './App.scss'
 
 
 function App() {
   const routeResult = useRoutes(routes);
   return (
-    <div className={styles.container}>
+    <>
       <Nav />
       {routeResult}
-    </div>
+    </>
   );
 }
 
