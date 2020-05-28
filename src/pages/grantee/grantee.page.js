4a9c0f2d947f3d8c0699a16b9e9f5659e21b2bfd
Johseffer <johseffer@gmail.com>
2020-05-28 16:23:33 -0300

    including grantee page actions header

--- src/pages/grantee/grantee.page.js
+++ src/pages/grantee/grantee.page.js
@@ -1,11 +1,93 @@
-import React from 'react';
+import React from 'react'
+import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import { withStyles } from '@material-ui/core/styles'
 
-const GranteePage = () => {
+import AddIcon from '@material-ui/icons/Add'
+import SearchIcon from "@material-ui/icons/Search"
+
+import './grantee.page.scss'
+
+const CssTextField = withStyles({
+    root: {
+      '& label.Mui-focused': {
+        color: '#1FBFAE',
+      },
+      '& .MuiInput-underline:after': {
+        borderBottomColor: '#1FBFAE',
+      },
+      '& .MuiOutlinedInput-root': {
+        '&.Mui-focused fieldset': {
+          borderColor: '#1FBFAE',
+        },
+      },
+    },
+  })(TextField);
+
+const styles = {
+    input: {
+        width: '205px',
+        backgroundColor: 'white',
+        '&::placeholder': {
+            color: 'gray',
+            opacity: '1'
+        }
+    },
+    adornedEnd: {
+        backgroundColor: 'white',
+        paddingRight: '0'
+    },
+    button: {
+        backgroundColor: 'white',
+        height: '40px',
+        marginLeft: '10px',
+        boxShadow: 'none',
+        minWidth: '42px',
+        '& > span > span': {
+            marginRight: '0px'
+        },
+        '&:hover': {
+            backgroundColor: '#1FBFAE',
+            boxShadow: 'none'
+        }
+    }
+};
+
+const GranteePage = (props) => {
+    const { classes } = props;
     return (
-        <div>
-            <p>Seus Favorecidos</p>
+        <div className="page-header">
+            <div className="action-container">
+                <span className="page-header-title">Seus Favorecidos</span>
+                <IconButton color="primary" size="small" className="add-button" aria-label="Incluir favorecido">
+                    <AddIcon />
+                </IconButton>
+            </div>
+            <div className="search-container">                                    
+                <CssTextField
+                    size="small"
+                    className="search-input"
+                    variant="outlined"
+                    id="custom-css-outlined-input"
+                    placeholder="Nome, CPF, agência ou conta"
+                    InputProps={{
+                        endAdornment: (
+                        <InputAdornment>
+                            <Button
+                                variant="contained"
+                                className={classes.button}
+                                startIcon={<SearchIcon />}
+                            />                            
+                        </InputAdornment>
+                        ),
+                        classes: { input: classes.input, adornedEnd: classes.adornedEnd }
+                    }}   
+                />
+            </div>
         </div>
     )
 }
 
-export default GranteePage
+export default withStyles(styles)(GranteePage);
