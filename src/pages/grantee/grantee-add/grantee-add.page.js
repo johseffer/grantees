3ee2ddfc90d03b1feef37fd1ae4a8432b90fc6ad
Johseffer <johseffer@gmail.com>
2020-06-01 12:34:16 -0300

    creating grantee add page

---
+++ src/pages/grantee/grantee-add/grantee-add.page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import GranteeForm from '../components/grantee-form/grantee-form.component'
+
+const GranteeAddPage = (props) => {
+    return (
+        <GranteeForm />
+    )
+}
+
+export default GranteeAddPage
