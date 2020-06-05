e8b0c56b08407ede2edfffe39c14435787f696a6
Johseffer <johseffer@gmail.com>
2020-06-05 18:12:55 -0300

    creating select account type component

---
+++ src/components/select-account-type/select-account-type.component.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import MenuItem from '@material-ui/core/MenuItem'
+import StyledTextField from './../styled-text-field/styled-text-field.component'
+
+
+const SelectAccountType = ({ onChange, selected, register, errors, bank }) => {
+    return (
+        <StyledTextField
+            label="Qual o tipo de conta?"
+            size="small"
+            style={{ width: '60%' }}
+            InputLabelProps={{ shrink: true }}
+            error={errors.accountType ? true : false}
+            inputProps={{
+                name: "accountType",
+                inputRef: (ref) => {
+                    if (!ref) return;
+                    register({ name: "accountType", value: selected });
+                }
+            }}
+            value={selected}
+            onChange={onChange}
+            helperText={errors.accountType ? errors.accountType.message : undefined}
+            select
+        >
+            <MenuItem value="1">Conta Corrente</MenuItem>
+            <MenuItem value="2">Conta Poupança</MenuItem>
+            {bank === '001' && <MenuItem value="3">Conta Fácil</MenuItem>}
+        </StyledTextField>
+    )
+}
+
+export default SelectAccountType
