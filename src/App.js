ca5b67252f9372db4cb6ed5912ac65530c0dfc94
Johseffer <johseffer@gmail.com>
2020-06-10 10:33:30 -0300

    including snackbar provider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useRoutes } from 'hookrouter'
+import { SnackbarProvider } from 'notistack'
+import IconButton from '@material-ui/core/IconButton'
+import CloseIcon from '@material-ui/icons/Close';
 import routes from './routes'
 
 import Nav from './components/nav/nav.component'
@@ -9,10 +12,21 @@ import './App.scss'
 
 function App() {
   const routeResult = useRoutes(routes);
+  const notistackRef = React.createRef();
+  const onClickDismiss = key => () => {
+    notistackRef.current.closeSnackbar(key);
+  }
+
   return (
     <>
-      <Nav />
-      {routeResult}
+      <SnackbarProvider maxSnack={3} ref={notistackRef} action={(key) => (
+        <IconButton aria-label="fechar" onClick={onClickDismiss(key)}>
+          <CloseIcon />
+        </IconButton>
+      )}>
+        <Nav />
+        {routeResult}
+      </SnackbarProvider>
     </>
   );
 }
